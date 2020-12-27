b1dde9fb78c939bf3dda63b6c52b5efa6ace8909
Himanshu Gupta <52830801+himanshu48@users.noreply.github.com>
2020-12-27 09:18:20 +0530

    Create insertionSort.js

---
+++ sorting/insertionSort.js
@@ -0,0 +1,21 @@
+// Time complexity (average & worst) - O(n2)
+// Time complexity (best) - O(n)
+// Space complexity - O(1)
+
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
+
+console.log(insertionSort([4,3,6,2,5]));
